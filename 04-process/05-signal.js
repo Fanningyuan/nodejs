ed496b12166d3e93e07f57ee2cead48f5c4ef22a
fanningyuan <1789624333@qq.com>
2019-09-04 19:04:48 +0800

    01

--- 04-process/05-signal.js
+++ 04-process/05-signal.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
 process.on('SIGINT',()=>{
-  
+  console.log('you process crtl+C or kill -2');
+  process.exit();
 });
 
-process.on('',()=>{
-  
-})
+process.on('SIGTSTP',()=>{
+  console.log('you press crtl+z');
+});
+
+process.stdin.resume();
